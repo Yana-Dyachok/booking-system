b560d16e98c81035f991c60833aae3a778b8a87e
Yana Diachok <annetart1994@gmail.com>
2025-04-29 22:47:13 +0300

    feat: add burger menu

--- frontend/src/shared/components/header/header.tsx
+++ frontend/src/shared/components/header/header.tsx
@@ -1,14 +1,21 @@
 'use client';
-import styles from './header.module.scss';
+
+import React, { useState } from 'react';
 import { LogOutButton } from '../log-out-button/log-out-button.component';
 import { ProfileSvg } from '@/shared/assets/svg/profile.svg';
 import { LogoSvg } from '@/shared/assets/svg/logo.svg';
 import { LinkNav } from '@/shared/ui/link-nav';
 import { useAuthStore } from '@/shared/store';
 import { Role } from '@/shared/types';
+import styles from './header.module.scss';
 
 export const Header = () => {
   const role = useAuthStore.getState().role;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
@@ -19,9 +26,30 @@ export const Header = () => {
         >
           <LogoSvg />
         </LinkNav>
-        <nav>
+        <button
+          className={styles.burgerMenu}
+          onClick={toggleMenu}
+          type="button"
+        >
+          <div
+            className={
+              isMenuOpen
+                ? `${styles.burgerIcon} ${styles.open}`
+                : styles.burgerIcon
+            }
+          >
+            <span className={styles.spanBurger} />
+            <span className={styles.spanBurger} />
+            <span className={styles.spanBurger} />
+          </div>
+        </button>
+        <nav
+          className={
+            isMenuOpen ? `${styles.navMain} ${styles.open}` : styles.navMain
+          }
+        >
           <ul className={styles.navList}>
-            <li>
+            <li onClick={() => setIsMenuOpen(false)}>
               {role === Role.CLIENT && (
                 <LinkNav
                   href="/appointments"
@@ -32,7 +60,7 @@ export const Header = () => {
                 </LinkNav>
               )}
             </li>
-            <li>
+            <li onClick={() => setIsMenuOpen(false)}>
               {role === Role.CLIENT && (
                 <LinkNav
                   href="/business"
@@ -43,17 +71,17 @@ export const Header = () => {
                 </LinkNav>
               )}
             </li>
-            <li>
+            <li onClick={() => setIsMenuOpen(false)}>
               <LinkNav
                 href="/profile"
                 activeClassName={styles.active}
                 className={styles.link}
               >
-                <ProfileSvg />
+                {isMenuOpen ? 'Profile' : <ProfileSvg />}
               </LinkNav>
             </li>
-            <li>
-              <LogOutButton />
+            <li className={styles.link} onClick={() => setIsMenuOpen(false)}>
+              {isMenuOpen ? 'Log out' : <LogOutButton />}
             </li>
           </ul>
         </nav>
