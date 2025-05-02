1f8e644595553c71e55bb01715ba9dab7f5cb597
Yana Diachok <annetart1994@gmail.com>
2025-05-02 13:46:00 +0300

    feat: add default token

--- backend/src/common/config/env-config.ts
+++ backend/src/common/config/env-config.ts
@@ -1,27 +1,29 @@
 import { registerAs } from '@nestjs/config';
 import { IConfig } from '../types/index';
 
-export const envConfig = registerAs(
-  'config',
-  (): IConfig => {
-    const jwtSecretKey = process.env.JWT_SECRET_KEY ?? '';
-    if (!jwtSecretKey) {
-      throw new Error('JWT_SECRET_KEY is not defined in environment variables.');
-    }
+export const envConfig = registerAs('config', (): IConfig => {
+	const jwtSecretKey = process.env.JWT_SECRET_KEY ?? '';
+	if (!jwtSecretKey) {
+		throw new Error(
+			'JWT_SECRET_KEY is not defined in environment variables.',
+		);
+	}
 
-    const jwtRefreshSecretKey = process.env.JWT_REFRESH_SECRET_KEY ?? '';
-    if (!jwtRefreshSecretKey) {
-      throw new Error('JWT_REFRESH_SECRET_KEY is not defined in environment variables.');
-    }
+	const jwtRefreshSecretKey = process.env.JWT_REFRESH_SECRET_KEY ?? '';
+	if (!jwtRefreshSecretKey) {
+		throw new Error(
+			'JWT_REFRESH_SECRET_KEY is not defined in environment variables.',
+		);
+	}
 
-    return {
-      port: parseInt(process.env.PORT as string, 10) ?? 3030,
-      secret_key: process.env.SECRET_KEY ?? 'secret-key',
-      token_expiration: parseInt(process.env.EXPIRATION_AGE as string, 10) ?? 3600,
-      nodeEnv: process.env.NODE_ENV ?? 'development',
-      frontendUrl: process.env.FRONTEND_URL,
-      jwtSecretKey,
-      jwtRefreshSecretKey,
-    };
-  },
-);
+	return {
+		port: parseInt(process.env.PORT as string, 10) ?? 3030,
+		secret_key: process.env.SECRET_KEY ?? 'secret-key',
+		token_expiration:
+			parseInt(process.env.EXPIRATION_AGE as string, 10) ?? 3600,
+		nodeEnv: process.env.NODE_ENV ?? 'development',
+		frontendUrl: process.env.FRONTEND_URL,
+		jwtSecretKey,
+		jwtRefreshSecretKey,
+	};
+});
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -22,8 +22,7 @@ export class AuthService {
 		private readonly jwtService: JwtService,
 		private readonly configService: ConfigService,
 	) {
-		this.jwtSecret =
-			this.configService.getOrThrow<string>('JWT_SECRET_KEY');
+		this.jwtSecret = this.configService.get<string>('JWT_SECRET_KEY') ?? 'default-secret-key';
 	}
 
 	async register(dto: RegisterUserDto): Promise<User> {
